1d91c2f35d650955681bcae0aaa71fdb3b4105cd
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-08-15 21:19:11 -0300

    Update aula1.py

--- semana01/aula1.py
+++ semana01/aula1.py
@@ -4,3 +4,23 @@ for line in range(len(matriz)):
     for column in range(len(matriz[line])):
         print(matriz[line][column], end=" ")
     print(end="\n")
+###########
+
+def scale(matriz,scale_value):
+    result = []
+    for line in range(len(matriz)):
+        result.append([])
+        for column in range(len(matriz[line])):
+            result[line].append(matriz[line][column] * scale_value)
+           
+    return result
+    
+    
+    
+    
+def main():
+    matriz = [[5,32,5],[3,76,43],[20,27,83]]
+    result = scale(matriz,5)
+    print(result)
+
+main()
