c9b0f5a7323dd5c0eed5f07c1e36314eb520d9f0
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-08-29 21:47:20 -0300

    Create exercicios02.py

---
+++ semana04/exercicios02.py
@@ -0,0 +1,3 @@
+#1. Escreva uma função que recebe uma lista de números e crie um dicionário que seja estruturado como sendo o
+#número a chave e a quantidade de vezes que o número está presente o valor. Exemplo: [1, 2, 3, 4, 5, 4, 3] => { 1: 1, 2:
+#1, 3: 2, 4: 2, 5: 1 }
