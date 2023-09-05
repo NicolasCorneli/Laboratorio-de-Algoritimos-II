df2a1e250f480ebd981f65327ad506cfef5eb1bd
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-09-05 20:01:13 -0300

    Update exercicios02.py

--- semana04/exercicios02.py
+++ semana04/exercicios02.py
@@ -74,3 +74,17 @@ def main():
     dic = a(dic)
     fmore(dic)
 main()
+#AGENDA
+#Escreva um programa para armazenar uma agenda de telefones em um dicionário. Cada pessoa pode ter um ou
+#mais telefones e a chave do dicionário é o nome da pessoa. Seu programa deve ter as seguintes funções:
+#createContact – essa função acrescenta um novo nome na agenda, com um ou mais telefones. Ela deve receber
+#como argumentos o nome e os telefones.
+#includePhone– essa função acrescenta um telefone em um nome existente na agenda. Caso o nome não exista
+#na agenda, você deve perguntar se a pessoa deseja incluí-lo. Caso a resposta seja afirmativa, use a função
+#anterior para incluir o novo nome.
+#deletePhone – essa função exclui um telefone de uma pessoa que já está na agenda. Se a pessoa tiver apenas
+#um telefone, ela deve ser excluída da agenda.
+#deleteContact – essa função exclui uma pessoa da agenda.
+#getPhones – essa função retorna os telefones de uma pessoa na agenda.
+
+
