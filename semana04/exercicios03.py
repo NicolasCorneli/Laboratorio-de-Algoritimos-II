b7fad67bd22724f750f32a50b164a616a9753dd5
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-09-05 16:19:01 -0300

    Update exercicios03.py

--- semana04/exercicios03.py
+++ semana04/exercicios03.py
@@ -129,3 +129,7 @@ def main():
     dic = whatever(dic,counterA,counterE,counterI,counterU,counterO)
     print("A quantide de vogais é:",(dic))
 main()
+#6.Escreva um programa que lê duas notas de vários alunos e armazena tais notas em um dicionário, onde a chave é
+#o nome do aluno. A entrada de dados deve terminar quando for lida uma string vazia como nome. Escreva uma
+#função que retorna a média do aluno, dado seu nome.
+
