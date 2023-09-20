6364705fc0ebb6f29623d35f61f922356f15eb4b
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-09-19 21:53:16 -0300

    Update aula07.py

--- semana07/aula07.py
+++ semana07/aula07.py
@@ -8,3 +8,24 @@ for i in range(len(list1)):
             list1[i] = z
             list1[x] = v
 print(list1)
+
+matrix = [[5,9,8],[4,3,7],[1,2,6]]
+for line in range(len(matrix)):
+    for column in range(len(matrix[line])):
+        min_line = line
+        min_column = column
+        
+        for line_aux in range(line,len(matrix)):
+            start = column if line == line_aux else 0
+            for column_aux in range(column, len(matrix[line_aux])):
+                if matrix[line_aux][column_aux] < matrix[min_line][min_column]:
+                    min_line =  line_aux
+                    min_column = column_aux
+        #Armazena o menor valor            
+        min_value = matrix[min_line][min_column]
+        #Armazena na posição do menor valor o valor atual
+        matrix[min_line][min_column] = matrix[line][column]
+        #Armazena no valor atual o menor valor
+        matrix[line][column] = min_value
+        
+print(matrix)
