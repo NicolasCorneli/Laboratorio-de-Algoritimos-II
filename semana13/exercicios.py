3fe3054b114877f06906aeb82d7a30ef674895f2
Nicolas <126731327+NicolasCorneli@users.noreply.github.com>
2023-11-07 22:07:34 -0300

    Update exercicios.py

--- semana13/exercicios.py
+++ semana13/exercicios.py
@@ -19,5 +19,28 @@ def main():
     find_element(greek_alphabet)
 
 main()
-    
+        
+#3. Escreva uma função calculate_square_root que aceite um número como entrada e
+#retorne a raiz quadrada desse número. Se o número for negativo, lance uma exceção
+#personalizada chamada NegativeNumberError com uma mensagem indicando que a
+#raiz quadrada de números negativos não é real. Para calcular a raiz utilize o operador
+#de exponenciação **. Exemplo: raiz = 81 ** (1/2)
+
+class NegativeNumberError(BaseException):
+    pass
+def calculate_square_root(number):
+    try:
+        if number < 0:
+            raise NegativeNumberError
+        else:    
+            raiz = number **(1/2)
+            print(raiz)
+    except NegativeNumberError:
+        print(f"[ERRO] A raiz quadrada de números negativos não é real")
+    except BaseException as error:
+        print(f"[ERRO] Ocorreu um erro: {error}")
+def main():
+    number = float(input("Digite um número: "))
+    calculate_square_root(number)
+main()
     
